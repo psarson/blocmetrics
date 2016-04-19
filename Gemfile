ca2dd39f42aaa8a246f916d96f666406b35f4fea
Patrick Sarson <sarsonmedia@gmail.com>
2016-04-18 21:02:38 -0400

    switch to postgres

--- Gemfile
+++ Gemfile
@@ -2,7 +2,7 @@ source 'https://rubygems.org'
 
 
 gem 'rails', '4.2.5.1'
-gem 'sqlite3'
+gem 'pg'
 gem 'sass-rails', '~> 5.0'
 gem 'uglifier', '>= 1.3.0'
 gem 'coffee-rails', '~> 4.1.0'
@@ -10,7 +10,6 @@ gem 'coffee-rails', '~> 4.1.0'
 gem 'jquery-rails'
 gem 'turbolinks'
 gem 'jbuilder', '~> 2.0'
-gem 'sdoc', '~> 0.4.0', group: :doc
 
 gem 'sdoc', '~> 0.4.0', group: :doc
 gem 'figaro'
---
+++ app/controllers/api/events-controller.rb
@@ -0,0 +1,35 @@
+class API::EventsController < ApplicationController
+
+   before_filter :set_access_control_headers
+
+   def set_access_control_headers
+
+     headers['Access-Control-Allow-Origin'] = '*'
+
+     headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS'
+
+     headers['Access-Control-Allow-Headers'] = 'Content-Type'
+   end
+
+   def preflight
+     head 200
+   end
+
+   def create
+     registered_application = RegisteredApplication.find_by(url: request.env['HTTP_ORIGIN'])
+
+     if registered_application.valid?
+       registered_application.save!
+       render json: registered_application.to_json, status: 201
+     else
+       render json: {error: "App is invalid", status: 400}, status: 400
+     end
+   end
+
+
+   private
+
+   def event_params
+     params.require(:event).permit(:name)
+   end
+ end
